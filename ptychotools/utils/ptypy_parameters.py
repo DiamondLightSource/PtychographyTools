bc99397577cd45b19f2a14b7b0efe57a87546d53
Aaron Parsons <aaron.parsons@diamond.ac.uk>
2019-05-02 13:03:48 +0100

    adds ability to read from yaml

--- ptychotools/utils/ptypy_parameters.py
+++ ptychotools/utils/ptypy_parameters.py
@@ -7,6 +7,7 @@ import ptypy.utils as u
 from ptypy.core import Ptycho
 import logging
 import json
+import yaml
 
 
 def _byteify(data, ignore_dicts = False):
@@ -38,10 +39,45 @@ def paramtree_to_json(paramtree, basefile, filepath):
 
     save_dict = {}
     save_dict['base_file'] = basefile
-    save_dict['parameter_tree'] = paramtree._to_dict()
+    save_dict['parameter_tree'] = paramtree._to_dict(Recursive=True)
     to_write_json = json.dumps(save_dict, indent=4, sort_keys=True)
     with open(filepath, 'w') as f:
         f.write(to_write_json)
+
+
+def paramtree_to_yaml(paramtree, basefile, filepath):
+    '''
+    generates a json file from a param tree
+    :param paramtree: The ptypy parameter tree to save out.
+    :param basefile: An existing ptyr file that we are basing this tree on.
+    :param filepath: the path the json file
+    :return: a Param based structure.
+    '''
+
+    save_dict = {}
+    save_dict['base_file'] = basefile
+    save_dict['parameter_tree'] = paramtree._to_dict(Recursive=True)
+    to_write_yaml = yaml.dump(save_dict)#, Dumper=yaml.SafeDumper)
+    with open(filepath, 'w') as f:
+        f.write(to_write_yaml)
+
+def paramtree_from_yaml(yaml_file):
+    '''
+    generates a ptypy param tree from a json file
+    :param json_file: the path the json file
+    :return: a Param based structure.
+    '''
+    in_dict = yaml.load(open(yaml_file), Loader=yaml.FullLoader)
+    parameters_to_run = u.Param()
+    if in_dict['base_file'] is not None:
+        logging.debug("Basing this scan off of the scan in {}".format(in_dict['base_file']))
+        previous_scan = Ptycho.load_run(in_dict['base_file'], False)  # load in the run but without the data
+        previous_parameters = previous_scan.p
+        parameters_to_run.update(previous_parameters)
+    if in_dict['parameter_tree'] is not None:
+        parameters_to_run.update(in_dict['parameter_tree'], Convert=True)
+    return parameters_to_run
+
 
 
 def paramtree_from_json(json_file):
